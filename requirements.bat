6d344f325c1289d308164fbdd31979c7ff1ffe74
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-06 03:49:01 +0530

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Installing dependencies...
+
+pip install face_recognition
+pip install opencv-python
+pip install numpy
+pip install glob
+pip install json
+pip install os
+pip install datetime
+
+echo Dependencies installed.
